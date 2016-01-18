614bf438ee735822853e0603984b2daf75c261fd
Davesmall28 <dave@dsmall.co.uk>
2016-01-18 21:08:12 +0000

    Increment .Net version to v4.5.2.

--- DSmall.UnitTest.Core/NuGetPush.bat
+++ DSmall.UnitTest.Core/NuGetPush.bat
@@ -1,5 +1,5 @@
 ..\NuGet\Nuget.exe pack DSmall.UnitTest.Core.csproj -Build -symbol
 
-..\NuGet\NuGet push DSmall.UnitTest.Core.1.1.1.nupkg
+..\NuGet\NuGet push DSmall.UnitTest.Core.1.2.1.nupkg
 
 pause

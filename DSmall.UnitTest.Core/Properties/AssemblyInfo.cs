f4699aa10c3e4707bce34d614328196b830be53d
Davesmall28 <dave@dsmall.co.uk>
2016-01-18 19:44:10 +0000

    Removed specific version dependency of packages.

---
+++ DSmall.UnitTest.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DSmall.UnitTest.Core")]
+[assembly: AssemblyDescription("The core framework for unit tests. Using NUnit.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DSmall Solutions Ltd")]
+[assembly: AssemblyProduct("DSmall.UnitTest.Core")]
+[assembly: AssemblyCopyright("Copyright © DSmall Solutions Ltd 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("52c333a5-e831-4192-b58f-49f983f5802f")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyInformationalVersion("1.1.1")]
---
+++ README.md
@@ -0,0 +1,4 @@
+# DSmall.UnitTest.Core
+The core framework for unit tests from DSmall Solutions Ltd.
+
+[![Build Status](https://magnum.travis-ci.com/Davesmall28/DSmall.UnitTestCore.svg?token=syrh3amDgq9dtpK7qH4B&branch=master)](https://magnum.travis-ci.com/Davesmall28/DSmall.UnitTest.Core)

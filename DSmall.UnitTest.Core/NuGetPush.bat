1cfd2d3d0073164150fa324e1db3cb404018b1cb
Davesmall28 <dave@dsmall.co.uk>
2016-07-01 12:43:01 +0100

    Target Multiple framework versions .Net v4.0 and v4.5.

--- DSmall.UnitTest.Core/NuGetPush.bat
+++ DSmall.UnitTest.Core/NuGetPush.bat
@@ -1,5 +1,5 @@
-..\NuGet\Nuget.exe pack DSmall.UnitTest.Core.csproj -Build -symbol
+..\NuGet\Nuget.exe pack DSmall.UnitTest.Core.csproj -Build -symbol -Properties Configuration=Release
 
-..\NuGet\NuGet push DSmall.UnitTest.Core.1.2.1.nupkg
+..\NuGet\NuGet push DSmall.UnitTest.Core.1.2.2.nupkg
 
 pause

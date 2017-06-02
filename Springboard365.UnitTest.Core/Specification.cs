21a05102d75ca607809dc03eb97f3d17908b716a
Davesmall28 <dave@dsmall.co.uk>
2017-06-02 17:33:01 +0100

    Remove Specification class.

--- Springboard365.UnitTest.Core/Specification.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Springboard365.UnitTest.Core
-{
-    public abstract class Specification<TUnderTest> : SpecificationBase
-        where TUnderTest : class
-    {
-        public SpecificationFixtureBase<TUnderTest> TestFixture { get; set; }
-    }
-}

50e27571c37c8b48214122e5163ce3ac5c5b4882
davesmall28 <dave@dsmall.co.uk>
2020-06-14 22:40:09 +0100

    Update full package to .Net v4.6.2 and slimline the build process.

--- Springboard365.UnitTest.Core/NuGet/NuGetPush.bat
+++ Springboard365.UnitTest.Core/NuGet/NuGetPush.bat
@@ -1,7 +1,7 @@
-SET packageVersion=1.2.0
+SET packageVersion=2.0.0
 
-NuGet.exe pack ../Springboard365.UnitTest.Core.csproj -Build -symbols -Version %packageVersion%
+NuGet.exe pack ../Springboard365.UnitTest.Core.csproj -Build -Symbols -Version %packageVersion% -Properties Configuration=Release
 
-NuGet.exe push Springboard365.UnitTest.Core.%packageVersion%.nupkg
+NuGet.exe push Springboard365.UnitTest.Core.%packageVersion%.nupkg -source "https://api.nuget.org/v3/index.json"
 
 pause
